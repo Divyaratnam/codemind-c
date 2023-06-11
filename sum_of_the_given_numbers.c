6b4fd91f8fd57579f3c8d31dbf3f161ca63a6e28
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2023-06-11 15:14:22 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     while(n!=0)     {         int a,b;         scanf("%d%d",&a,&b);         printf("%d ",a+b);         n--;     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        printf("%d
+",a+b);
+        n--;
+    }
+}
